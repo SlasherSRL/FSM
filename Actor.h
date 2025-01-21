77a33f99e9f0c42bfff7be6df64eed22e498b6a4
Dennis Karlsson <89897864+SlasherSRL@users.noreply.github.com>
2025-01-21 13:28:25 +0100

    Update Actor.h

--- Actor.h
+++ Actor.h
@@ -10,19 +10,20 @@ struct Job
 {
     JobType type;
     int incomeRate;
-    int hungerRate;
-    int thirstRate;
-    int energyRate;
+    float hungerRate;
+    float thirstRate;
+    float energyRate;
     
 };
 Job CreateJob(JobType type)
 {
     switch(type)
     {
+        // job type, income rate, hunger rate, thirst rate, energy consumption rate
         case JobType::OFFICE_WORKER:
             return {type, 10, 1 / 10.0f, 1 / 3.0f, 1 / 2.0f};  // Hunger, Thirst, Energy rates
         case JobType::PILOT:
-            return {type, 15, 1 / 8.0f, 1 / 2.0f, 1 / 3.0f};  // Pilots thirst more and tire faster
+            return {type, 20, 1 / 8.0f, 1 / 2.0f, 1 / 3.0f};  // Pilots thirst more and tire faster
         case JobType::UNEMPLOYED:
             return {type, 0, 1 / 15.0f, 1 / 5.0f, 1 / 4.0f};  // Unemployed have slower rates
         default:
