1d6d4855ecb8cbe9fe3c91ffc48a81d1b766eb1f
Dennis Karlsson <89897864+SlasherSRL@users.noreply.github.com>
2025-01-26 12:19:11 +0100

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -124,7 +124,7 @@ void Actor::PrintStatus()const
     	std::cout << "| Socialized : " << std::setw(8) << socialized << " / 100 |\n";
     	std::cout << "| Money      : $" << std::setw(16) << money << " |\n";
 	std::cout << "| Food     : " << std::setw(16) << food << " |\n";
-	std::cout << "| Giftcards      : $" << std::setw(16) << giftcards << " |\n";
+	std::cout << "| Giftcards      : $" << std::setw(16) << giftCards << " |\n";
     	std::cout << "+------------------------------+\n";
 }
 
